556f540f6db5c5772011349422c971119fbb8454
Christophe-Ringot <christophe.ringot1996@gmail.com>
2025-09-29 14:41:43 +0200

    Data

---
+++ demos_spark/src/main/scala/demos/Demo_rdd_films.scala
@@ -0,0 +1,34 @@
+package demos
+
+import org.apache.spark.sql.SparkSession
+
+object Demo_rdd_films {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().master("local[*]").appName("demo-film").getOrCreate()
+
+    val sc = spark.sparkContext
+
+    sc.setLogLevel("WARN")
+
+    // Lecture du fichier film.data
+    val rddFilm = sc.textFile("./Data/film.data")
+
+    // Extraction de la valeur qui nous intéresse (3éme colonne)
+    // 196	242	3	881250949
+    // 196,242,3,881250949
+    val rddNotes = rddFilm.map(line => line.split("\t")(2))
+
+    // Affichage du type RDD
+    println(rddNotes)
+
+    // On compte le nombre d’éléments par valeur.
+    val result = rddNotes.countByValue()
+
+    // tri et affichage du resultat
+    result.toSeq.sortBy(_._1).foreach {
+      case (key, value) =>
+        println(s"$key : $value")
+    }
+
+  }
+}

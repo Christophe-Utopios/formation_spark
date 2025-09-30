0a43cdf82d0ece99f02cb6fb73cac6c30eed8da1
Christophe-Ringot <christophe.ringot1996@gmail.com>
2025-09-30 16:28:09 +0200

    Exercice 05

---
+++ 02_spark/demos_spark/src/main/scala/demos/Demo_broadcast.scala
@@ -0,0 +1,55 @@
+package demos
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object Demo_broadcast {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().master("local[*]").appName("demo-broadcast").getOrCreate()
+
+    import spark.implicits._
+
+    val countryDf = Seq(
+      ("US", "United States"),
+      ("FR", "France"),
+      ("DE", "Germany"),
+      ("JP", "Japan")
+    ).toDF("country_code", "country_name")
+
+    val userDf = Seq(
+      ("Toto", "US", 30),
+      ("Tata", "FR", 20),
+      ("Titi", "DE", 24),
+      ("Tutu", "FR", 28)
+    ).toDF("name", "country_code", "age")
+
+    // Broadcast join (Spark optimise automatiquement les petites tables)
+    val enrichedDf = userDf.join(
+      broadcast(countryDf),
+      Seq("country_code")
+    )
+
+    // Broadcast de variable de config
+
+    case class FilterConfig(
+                              minAge: Int,
+                              allowedCountries: Set[String]
+                           )
+
+    val config = FilterConfig(
+      minAge = 25,
+      allowedCountries = Set("FR", "US")
+    )
+
+    val broadcastConfig = spark.sparkContext.broadcast(config)
+
+    val userFilterUDF = udf((age: Int, country: String) => {
+      val conf = broadcastConfig.value
+      age >= conf.minAge && conf.allowedCountries.contains(country)
+    })
+
+    val filteredUserDF = userDf.filter(userFilterUDF(col("age"), col("country_code")))
+
+    filteredUserDF.show()
+  }
+}
